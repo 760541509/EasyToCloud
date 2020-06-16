310d143eb41a020526e99aec37f80511bd3ab524
胖虎 <760541509@qq.com>
2020-06-16 09:41:29 +0800

    2020-6-16

--- system/src/main/java/com/yingtongquan/system/equipment/controller/TblEquipmentController.java
+++ system/src/main/java/com/yingtongquan/system/equipment/controller/TblEquipmentController.java
@@ -1,7 +1,9 @@
 package com.yingtongquan.system.equipment.controller;
 
 import com.yingtongquan.startcommon.base.ResultVo;
+import com.yingtongquan.system.equipment.pojo.DeleteEquipment;
 import com.yingtongquan.system.equipment.pojo.ShopEquipment;
+import com.yingtongquan.system.equipment.pojo.UpdateEquipment;
 import com.yingtongquan.system.equipment.service.TblEquipmentService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -22,8 +24,20 @@ public class TblEquipmentController {
 
     @ApiOperation(value = "添加设备")
     @PostMapping("/addShopEquipment")
-    public ResultVo addShopEquipment(@RequestBody ShopEquipment shopEquipment){
+    public ResultVo addShopEquipment(@RequestBody ShopEquipment shopEquipment) {
         return ResultVo.successResult(equipmentService.addShopEquipment(shopEquipment));
+    }
+
+    @ApiOperation(value = "修改设备")
+    @PostMapping("/updateShopEquipment")
+    public ResultVo updateShopEquipment(@RequestBody UpdateEquipment updateEquipment) {
+        return ResultVo.successResult(equipmentService.updateShopEquipment(updateEquipment));
+    }
+
+    @ApiOperation(value = "删除设备")
+    @PostMapping("/deleteShopEquipment")
+    public ResultVo deleteShopEquipment(@RequestBody DeleteEquipment deleteEquipment) {
+        return ResultVo.successResult(equipmentService.deleteShopEquipment(deleteEquipment));
     }
 
 }
